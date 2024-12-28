a3057602112d83eb6512b4c4d1fece9cbde3d699
bmarshall07 <benmarsh33@hotmail.co.uk>
2024-12-28 10:51:51 +0000

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,1 +1,10 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALxTMkfLf_n-BttEsmJWuSZBAloRM0RQU",
+  authDomain: "energyconsult-a0555.firebaseapp.com",
+  projectId: "energyconsult-a0555",
+  storageBucket: "energyconsult-a0555.firebasestorage.app",
+  messagingSenderId: "378868227507",
+  appId: "1:378868227507:web:1688a3f4e29d67f25affc6",
+  measurementId: "G-4ZS18E2DRY"
+};
