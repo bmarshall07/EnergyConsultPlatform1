f887f7e98fcbfd69b747db6f1f6ed9ca670d0957
bmarshall07 <benmarsh33@hotmail.co.uk>
2024-12-28 12:07:53 +0000

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,19 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your Firebase configuration object
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB5yIyAhB9fbbaXTW03GRBIuU58XUe1q3E",
+  authDomain: "energyconsult-7a837.firebaseapp.com",
+  databaseURL: "https://energyconsult-7a837-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "energyconsult-7a837",
+  storageBucket: "energyconsult-7a837.firebasestorage.app",
+  messagingSenderId: "131060027292",
+  appId: "1:131060027292:web:1bf6fe415c06214f5d8259",
+  measurementId: "G-FT6CQTN0P2"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-export { database, ref, set };
+const analytics = getAnalytics(app);
